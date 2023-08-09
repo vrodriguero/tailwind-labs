c2515f4335afeb8487dc4798971cec798e3e3d40
vrodriguero <vickyamauti@hotmail.com>
2023-08-09 17:31:25 +1000

    level one animations

--- resources/js/Pages/Game.vue
+++ resources/js/Pages/Game.vue
@@ -1,6 +1,15 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
-import GameLayout from "@/Layouts/GameLayout.vue";
+import { Head } from '@inertiajs/vue3'
+import GameLayout from "@/Layouts/GameLayout.vue"
+import Level1 from "@/Components/Game/Level1.vue"
+
+const props = defineProps({
+    level: {
+        type: Number,
+        required: true,
+        default: 1,
+    },
+});
 </script>
 
 <template>
@@ -8,10 +17,10 @@ import GameLayout from "@/Layouts/GameLayout.vue";
 
     <GameLayout>
         <template #canvas>
-            Animation goes here
+            <Level1 />
         </template>
         <div class="text-white">
-         hello?
+            <button @click="startIdleAnimation" class="bg-gray-500 text-white px-4 py-2 rounded-md z-50">Level Completed ({{ level }})</button>
         </div>
     </GameLayout>
 </template>
